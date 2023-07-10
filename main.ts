f8538c5aab790a57f6bc3924fa18f16379054df4
Pavel Frankov <pavel@frankov.ru>
2023-07-10 20:36:21 +0300

    First version of Obsidian Colored Tags Plugin

--- main.ts
+++ main.ts
@@ -1,137 +1,209 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, MarkdownView } from "obsidian";
+import Color from "colorjs.io";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
+interface ColoredTagsPluginSettings {
 	mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const DEFAULT_SETTINGS: ColoredTagsPluginSettings = {
+	mySetting: "default",
+};
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class ColoredTagsPlugin extends Plugin {
+	settings: ColoredTagsPluginSettings;
+	updateDebounce: NodeJS.Timeout;
+	tagsSet: Set<string> = new Set();
 
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+		this.registerEvent(
+			this.app.workspace.on("editor-change", () => {
+				this.updateDebounce && clearTimeout(this.updateDebounce);
+				this.updateDebounce = setTimeout(() => {
+					const view =
+						this.app.workspace.getActiveViewOfType(MarkdownView);
+					if (view?.contentEl) {
+						this.update(this.getTagsFromDOM(view.contentEl));
 					}
+				}, 300);
+			})
+		);
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+		this.tagsSet.clear();
+		this.update(this.getTagsFromApp());
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.registerEvent(
+			this.app.workspace.on("active-leaf-change", () => {
+				this.tagsSet.clear();
+				this.update(this.getTagsFromApp());
+			})
+		);
+	}
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+	getTagsFromDOM(domEl: HTMLElement): string[] {
+		return Array.from(domEl.querySelectorAll(".tag, .cm-hashtag-end"))
+			.map((tagEl: HTMLElement) => tagEl.innerText.replace(/\#/g, ""))
+			.filter((x) => x);
+	}
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+	getTagsFromApp(): string[] {
+		return Object.keys(this.app.metadataCache.getTags())
+			.map((tag) => {
+				const tagName = tag.replace(/\#/g, "");
+
+				return tagName;
+			})
+			.filter((x) => x.length);
+	}
+
+	update(tagsList: string[]) {
+		if (tagsList.find((tag) => !this.tagsSet.has(tag))) {
+			tagsList.forEach((tag) => {
+				this.tagsSet.add(tag);
+			});
+			colorizeTags(Array.from(this.tagsSet.keys()));
+		}
 	}
 
 	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
+		this.updateDebounce && clearTimeout(this.updateDebounce);
+		this.tagsSet.clear();
+		removeCSS();
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+function darkenColorForContrast(baseColor, contrast = 4.5) {
+	const colorLight = new Color(baseColor).to("lch");
+	const colorDark = new Color(baseColor).to("lch");
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+	colorLight.c = Math.max(colorLight.c + 3, 0);
+	colorDark.c = Math.max(colorDark.c + 20, 0);
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+	for (let i = 0; i < 100; i++) {
+		if (
+			colorLight.contrastAPCA(baseColor) >= 60 &&
+			colorLight.contrastWCAG21(baseColor) > contrast
+		) {
+			return colorLight.toString();
+		}
+		if (
+			colorDark.contrastAPCA(baseColor) <= -60 &&
+			colorDark.contrastWCAG21(baseColor) > contrast
+		) {
+			return colorDark.toString();
+		}
+
+		colorLight.l++;
+		colorDark.l--;
 	}
+	return "#f00";
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+function getColors(
+	input: string,
+	isDarkTheme: boolean
+): { background: string; color: string } {
+	const chunks = input.split("/");
+	const background =
+		chunks
+			.reduce((acc, chunk, i) => {
+				const color = generateUniqueColor(chunk, isDarkTheme);
+				if (acc) {
+					return acc.mix(
+						color,
+						((chunks.length - i) / chunks.length) * 0.3 + 0.2
+					);
+				}
+				return new Color(color);
+			}, null)
+			?.to("lch")
+			.toString({ format: "lch" }) || "#000";
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
+	const color = darkenColorForContrast(background);
+
+	return { background, color };
+}
+
+function colorizeTags(tagNames: string[]) {
+	tagNames = tagNames.map((tag) => tag.replace(/\#/g, ""));
+
+	const css = tagNames
+		.map((tagName) => {
+			const tagHref = "#" + tagName.replace(/\//g, "\\/");
+			const tagFlat = tagName.replace(/\//g, "");
+			
+			const { background: backgroundLight, color: colorLight } =
+				getColors(tagName, false);
+			const { background: backgroundDark, color: colorDark } = getColors(
+				tagName,
+				true
+			);
+			return `
+				body a.tag[href="${tagHref}"], body .cm-s-obsidian .cm-line span.cm-tag-${tagFlat}.cm-hashtag {
+					background-color: ${backgroundLight};
+					color: ${colorLight};
+				}
+				body.theme-dark a.tag[href="${tagHref}"], body.theme-dark .cm-s-obsidian .cm-line span.cm-tag-${tagFlat}.cm-hashtag {
+					background-color: ${backgroundDark};
+					color: ${colorDark};
+				}
+		`;
+		})
+		.join("\n");
+
+	insertCSS(css);
+}
+
+function generateUniqueColor(string: string, isDarkTheme: boolean) {
+	const colorPalette = generateColorPalette(isDarkTheme);
+
+	let hashCode = 0;
+	for (let i = 0; i < string.length; i++) {
+		hashCode = string.charCodeAt(i) + ((hashCode << 5) - hashCode);
 	}
 
-	display(): void {
-		const {containerEl} = this;
+	hashCode = Math.abs(hashCode) % colorPalette.length;
+	const selectedColor = colorPalette[hashCode];
 
-		containerEl.empty();
+	return selectedColor;
+}
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+function generateColorPalette(isDarkTheme: boolean) {
+	const PALETTE_SIZE = 36;
+	const BASE_CHROMA = 16;
+	const BASE_LIGHTNESS = 87;
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+	const hueIncrement = 360 / PALETTE_SIZE;
+
+	const colorPalette = [];
+	for (let i = 0; i < PALETTE_SIZE; i++) {
+		const hue = i * hueIncrement;
+
+		let chroma = BASE_CHROMA;
+		let lightness = BASE_LIGHTNESS;
+		if (isDarkTheme) {
+			chroma = Math.round(BASE_CHROMA * 1.8);
+			lightness = Math.round(BASE_LIGHTNESS / 2.5);
+		}
+
+		const lchColor = new Color("lch", [lightness, chroma, hue]).toString();
+		colorPalette.push(lchColor);
 	}
+
+	return colorPalette;
+}
+
+function insertCSS(css: string): void {
+	removeCSS();
+	document.head
+		.createEl("style", {
+			type: "text/css",
+			attr: { "colored-tags-style": "" },
+		})
+		.setText(css);
+}
+
+function removeCSS(): void {
+	document.head.querySelectorAll('[colored-tags-style]').forEach((el) => {
+		el.remove();
+	});
 }
